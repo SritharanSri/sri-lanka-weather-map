6fc75fbe20cbe4b771562b806375d2f32fb6189b
SritharanSri <ssrikalai2255@gmail.com>
2024-04-05 21:15:23 +0530

    Frontend update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/register" element={<RegistrationPage />} />
         <Route
-          path="/login"
+          path="/"
           element={<LoginPage /> } 
         />
       </Routes>
